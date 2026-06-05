2354ed4c65d08209acdcf4ec6f88c0ae859e076a
bcpethum <bcpethum@gmail.com>
2026-06-05 19:34:05 +0530

    feat: initialize Express server with CORS, database connection, and API routes

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.get("/", (req, res) => {
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
